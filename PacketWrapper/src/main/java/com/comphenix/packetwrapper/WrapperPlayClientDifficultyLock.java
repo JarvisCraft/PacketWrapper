3cf879633b93770c0b827d2b122cdd11f6d76eb5
progrm_jarvis <gh@progrm-jarvis.ru>
2020-12-05 23:32:55 +0300

    BackwardsCompat: WrapperPlayClientDifficultyLock

--- PacketWrapper/src/main/java/com/comphenix/packetwrapper/WrapperPlayClientDifficultyLock.java
+++ PacketWrapper/src/main/java/com/comphenix/packetwrapper/WrapperPlayClientDifficultyLock.java
@@ -18,9 +18,11 @@
  */
 package com.comphenix.packetwrapper;
 
+import com.comphenix.packetwrapper.util.BackwardsCompatible;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketContainer;
 
+@BackwardsCompatible(sinceMinor = 14)
 public class WrapperPlayClientDifficultyLock extends AbstractPacket {
 
     public static final PacketType TYPE = PacketType.Play.Client.DIFFICULTY_LOCK;
