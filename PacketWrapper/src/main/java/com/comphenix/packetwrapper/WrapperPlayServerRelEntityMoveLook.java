5875ca1c1a4fac88746676bed56251b9284c1ac9
progrm_jarvis <gh@progrm-jarvis.ru>
2020-12-06 21:19:57 +0300

    BackwardsCompat: WrapperPlayServerRelEntityMoveLook

--- PacketWrapper/src/main/java/com/comphenix/packetwrapper/WrapperPlayServerRelEntityMoveLook.java
+++ PacketWrapper/src/main/java/com/comphenix/packetwrapper/WrapperPlayServerRelEntityMoveLook.java
@@ -18,14 +18,12 @@
  */
 package com.comphenix.packetwrapper;
 
-import org.bukkit.World;
-import org.bukkit.entity.Entity;
-
+import com.comphenix.packetwrapper.util.BackwardsCompatible;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.events.PacketEvent;
 
-public class WrapperPlayServerRelEntityMoveLook extends AbstractPacket {
+@BackwardsCompatible
+public class WrapperPlayServerRelEntityMoveLook extends WrapperPlayServerEntity {
 	public static final PacketType TYPE = PacketType.Play.Server.REL_ENTITY_MOVE_LOOK;
 
 	public WrapperPlayServerRelEntityMoveLook() {
@@ -37,98 +35,34 @@ public class WrapperPlayServerRelEntityMoveLook extends AbstractPacket {
 		super(packet, TYPE);
 	}
 
-	/**
-	 * Retrieve Entity ID.
-	 * <p>
-	 * Notes: entity's ID
-	 * 
-	 * @return The current Entity ID
-	 */
-	public int getEntityID() {
-		return handle.getIntegers().read(0);
-	}
-
-	/**
-	 * Set Entity ID.
-	 * 
-	 * @param value - new value.
-	 */
-	public void setEntityID(int value) {
-		handle.getIntegers().write(0, value);
-	}
-
-	/**
-	 * Retrieve the entity of the painting that will be spawned.
-	 * 
-	 * @param world - the current world of the entity.
-	 * @return The spawned entity.
-	 */
-	public Entity getEntity(World world) {
-		return handle.getEntityModifier(world).read(0);
-	}
-
-	/**
-	 * Retrieve the entity of the painting that will be spawned.
-	 * 
-	 * @param event - the packet event.
-	 * @return The spawned entity.
-	 */
-	public Entity getEntity(PacketEvent event) {
-		return getEntity(event.getPlayer().getWorld());
-	}
-
 	@Override
 	public double getDx() {
-/* TODO(begin) */
-		return handle.getShorts().read(0) / 4096D;
-/* TODO(mid) */
 		return super.getDx();
-/* TODO(end) */
 	}
 
 	@Override
 	public void setDx(double value) {
-/* TODO(begin) */
-		handle.getShorts().write(0, (short) (value * 4096));
-/* TODO(mid) */
 		super.setDx(value);
-/* TODO(end) */
 	}
 
 	@Override
 	public double getDy() {
-/* TODO(begin) */
-		return handle.getShorts().read(1) / 4096D;
-/* TODO(mid) */
 		return super.getDy();
-/* TODO(end) */
 	}
 
 	@Override
 	public void setDy(double value) {
-/* TODO(begin) */
-		handle.getShorts().write(1, (short) (value * 4096));
-/* TODO(mid) */
 		super.setDy(value);
-/* TODO(end) */
 	}
 
 	@Override
 	public double getDz() {
-/* TODO(begin) */
-		return handle.getShorts().read(2) / 4096D;
-/* TODO(mid) */
 		return super.getDz();
-/* TODO(end) */
 	}
 
 	@Override
 	public void setDz(double value) {
-/* TODO(begin) */
-		handle.getShorts().write(2, (short) (value * 4096));
-/* TODO(mid) */
 		super.setDz(value);
-/* TODO(end) */
 	}
 
 	@Override
